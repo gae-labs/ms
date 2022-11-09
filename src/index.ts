b5760fe888c10a0f33525d3a9c9a95991ae92d33
Morgan Finney <dev@paradox-glitch.co.uk>
2022-11-09 16:18:09 +0000

    [~Change] Cleaned Up

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,7 @@ function msFn(value: StringValue | number, options?: Options): number | string {
 function parse(str: string): number {
 
   const l_Regex =
-    /^(?<years>(?<yearsValue>-?([0-9]\d*)?(\.\d+)?)(\s+)?(?<yearsType>years?|yrs?|y))?(\s+)?(?<weeks>(?<weeksValue>-?([0-9]\d*)?(\.\d+)?)(\s+)?(?<weeksType>weeks?|w))?(\s+)?(?<days>(?<daysValue>-?([0-9]\d*)?(\.\d+)?)(\s+)?(?<daysType>days?|d))?(\s+)?(?<hours>(?<hoursValue>-?([0-9]\d*)?(\.\d+)?)(\s+)?(?<hoursType>hours?|hrs?|h))?(\s+)?(?<mins>(?<minsValue>-?([0-9]\d*)?(\.\d+)?)(\s+)?(?<minsType>minutes?|mins?|m(?!s|i)))?(\s+)?(?<secs>(?<secsValue>-?([0-9]\d*)?(\.\d+)?)(\s+)?(?<secsType>seconds?|secs?|s))?(\s+)?(?<msecs>(?<msecsValue>-?([0-9]\d*)?(\.\d+)?)(\s+)?(?<msecsType>milliseconds?|msecs?|ms|$))?/gmi
+    /^((?<yearsValue>-?(\d*)?(\.\d+)?)(\s+)?(>years?|yrs?|y))?(\s+)?((?<weeksValue>-?(\d*)?(\.\d+)?)(\s+)?(weeks?|w))?(\s+)?((?<daysValue>-?(\d*)?(\.\d+)?)(\s+)?(days?|d))?(\s+)?((?<hoursValue>-?(\d*)?(\.\d+)?)(\s+)?(hours?|hrs?|h))?(\s+)?((?<minsValue>-?(\d*)?(\.\d+)?)(\s+)?(minutes?|mins?|m(?!s|i)))?(\s+)?((?<secsValue>-?(\d*)?(\.\d+)?)(\s+)?(seconds?|secs?|s))?(\s+)?((?<msecsValue>-?(\d*)?(\.\d+)?)(\s+)?(milliseconds?|msecs?|ms|$))?/gim;
 
   const l_Match = l_Regex.exec(
     str,
@@ -96,26 +96,21 @@ function parse(str: string): number {
 
   const l_Groups = l_Match?.groups as { yearsValue?: string; weeksValue?: string; daysValue?: string; hoursValue?: string; minsValue?: string; secsValue?: string; msecsValue?: string; } | undefined;
 
-  if (!l_Groups) {
-    return NaN;
-  }
-
-  if (l_Groups.yearsValue == undefined && l_Groups.weeksValue == undefined && l_Groups.daysValue == undefined && l_Groups.hoursValue == undefined && l_Groups.minsValue == undefined && l_Groups.secsValue == undefined && l_Groups.msecsValue == undefined) {
+  if (!l_Groups || l_Groups.yearsValue == undefined && l_Groups.weeksValue == undefined && l_Groups.daysValue == undefined && l_Groups.hoursValue == undefined && l_Groups.minsValue == undefined && l_Groups.secsValue == undefined && l_Groups.msecsValue == undefined) {
     return NaN;
   }
 
   let l_TotalMS = 0;
 
-  l_TotalMS += parseFloat((l_Groups.yearsValue != undefined) ? l_Groups.yearsValue : `0`) * y
-  l_TotalMS += parseFloat((l_Groups.weeksValue != undefined) ? l_Groups.weeksValue : `0`) * w
-  l_TotalMS += parseFloat((l_Groups.daysValue != undefined) ? l_Groups.daysValue : `0`) * d
-  l_TotalMS += parseFloat((l_Groups.hoursValue != undefined) ? l_Groups.hoursValue : `0`) * h
-  l_TotalMS += parseFloat((l_Groups.minsValue != undefined) ? l_Groups.minsValue : `0`) * m
-  l_TotalMS += parseFloat((l_Groups.secsValue != undefined) ? l_Groups.secsValue : `0`) * s
-  l_TotalMS += parseFloat((l_Groups.msecsValue != undefined) ? l_Groups.msecsValue : `0`)
+  l_TotalMS += parseFloat((l_Groups.yearsValue != undefined) ? l_Groups.yearsValue : `0`) * y;
+  l_TotalMS += parseFloat((l_Groups.weeksValue != undefined) ? l_Groups.weeksValue : `0`) * w;
+  l_TotalMS += parseFloat((l_Groups.daysValue != undefined) ? l_Groups.daysValue : `0`) * d;
+  l_TotalMS += parseFloat((l_Groups.hoursValue != undefined) ? l_Groups.hoursValue : `0`) * h;
+  l_TotalMS += parseFloat((l_Groups.minsValue != undefined) ? l_Groups.minsValue : `0`) * m;
+  l_TotalMS += parseFloat((l_Groups.secsValue != undefined) ? l_Groups.secsValue : `0`) * s;
+  l_TotalMS += parseFloat((l_Groups.msecsValue != undefined) ? l_Groups.msecsValue : `0`);
 
   return l_TotalMS;
-
 }
 
 // eslint-disable-next-line import/no-default-export
